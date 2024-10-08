91ef86140b9064eff372c4f8e44f03a031041903
MYXAMOR338 <silova155@gmail.com>
2024-10-08 12:07:01 +0300

    styles ++ ++

--- main.cpp
+++ main.cpp
@@ -6,8 +6,10 @@ int main(int argc, char *argv[]) {
 
     // Создаем виджет игры
     GameWidget game;
+    game.setWindowTitle("Sink the Ship!");
     game.resize(400, 470);  // Задаем размер окна игры
     game.show();
+
 
     return app.exec();  // Запускаем главный цикл приложения
 }
